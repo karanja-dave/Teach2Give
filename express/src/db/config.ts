e00c02b5f24edd5cd52c7affd03554bb41bbd031
karanjadave <davekaranja6@gmail.com>
2025-10-10 10:51:22 +0300

    connected express server with the mssql database server

---
+++ express/src/db/config.ts
@@ -0,0 +1,70 @@
+// load required packages
+import dotenv from 'dotenv' //used to load variables from the .env file
+import assert from 'assert' //used to check if allrequired variables exists
+import sql from 'mssql'
+
+//laod enviroment variables
+dotenv.config()
+
+// extract variables from the process.env
+//ectracted variables are used to connect the express server with a DataBase
+const{
+    SQL_SERVER,
+    SQL_USER,
+    SQL_PWD,
+    SQL_DB,
+    PORT
+}=process.env
+
+/**
+ * the above code(line 9-15) is shorthand for :
+ *  const SQL_SERVER = process.env.SQL_SERVER;
+    const SQL_USER = process.env.SQL_USER;
+    const SQL_PWD = process.env.SQL_PWD;
+    const SQL_DB = process.env.SQL_DB;
+    const PORT = process.env.PORT;
+    
+ */
+
+// ensure all enviroment variables are defined
+assert(PORT,"PORT is required")
+assert(SQL_SERVER,"SQL_SERVER is required")
+assert(SQL_USER,"SQL_USER required")
+assert(SQL_PWD,"SQL_PWD required")
+assert(SQL_DB,"SQL_DB required")
+
+// configuration object for DataBase connection 
+
+export const config={ //createt and export the config object
+    port:PORT,
+    sqlConfig:{ //define credentials required to connect to DataBase (mssql)
+        user:SQL_USER,
+        password:SQL_PWD,
+        database:SQL_DB,
+        server:SQL_SERVER,
+        // pool is used to manage  multiple connections to a database 
+        pool:{
+            max:10, //max number of 10 connections at same time, each connection handles one query
+            min:0, //no min number of connections
+            idleTimeoutMillis: 30000  //closes a connection that is idle for 30 seconds 
+        },
+        options:{
+            encrypt:true, //encrypts data transfered to and from SQL and express
+            trustServerCertificate: true //tells driver to trust  server's certificate 
+        }
+
+    }
+};
+
+//getPool function is used to connect the Expres server to the MSQL database server
+export const getPool =async () => { //create and export the getPool function
+
+    try{//handles what happens on succesful connectionc
+        const pool = await sql.connect(config.sqlConfig) //connect to SQL server ans store the active connection in the pool object
+        return pool; //return the pool object on succesfull connection that store the active connection 
+
+    } catch(error) {//hanldes what happens on error encounter
+        console.log("SQL Connection error",error) //message to be displayed on error 
+        throw error //displays the error encountered
+    }
+}
--- express/src/index.ts
+++ express/src/index.ts
@@ -2,11 +2,16 @@
 //load express package 
 import express from 'express';
 
+import { getPool } from './db/config'; //load getPool function defined in config={.ts file}
+import { get } from 'http';
+import { pool } from 'mssql';
+
+
 // invocation - store all express functions and features inside the app object
 const app = express()
 
 // define the router handler function
-app.get('/test',(req,res)=>{ //since the req arg is not used replace it with an underscore instead
+app.get('/',(req,res)=>{ //since the req arg is not used replace it with an underscore instead
     res.send("Hello, the express server is up and running")
 })
 
@@ -17,3 +22,42 @@ const port=8081 //this is where the server will run
 app.listen(port,()=>{
     console.log(`Server is running on port: http://localhost:${port}`)
 })
+
+//fetch users  table
+app.get('/users',(req,res)=>{ //this line defines the route
+    getPool() //connect to database
+    .then(pool=>{ //if successful connection;
+        return pool.request().query('SELECT *FROM Users') //make request which is a querry to view the users table
+    }).then(result=>{ //if sql querry is a success, result object is returned from sql server
+        console.log("result",result); 
+        //result.recordset contains array of rows from user table
+        res.json(result.recordset) //sends data back to client in json format
+
+    }).catch(err=>{ //if any error occurs(ie connection issue/sql syntax error);
+        console.log("SQL error",err); //error is logged in the console
+        res.status(500).send("Server Error") //server sends a 500 internal server error with response message "Server Error"
+    })
+})  
+
+
+// fetch todo tables
+app.get('/todos',(req,res)=>{
+    getPool().then(pool=>{
+        return pool.request().query('SELECT *FROM Todos')
+    }).then(result=>{
+        console.log("result",result);
+        res.json(result.recordset)
+    }).catch(err=>{
+        console.log("SQL error",err);
+        res.status(500).send("Server Error")
+    })
+}) 
+
+
+
+
+
+//check if erver has successfully connect to DB, should be run before the codeblock above it 
+getPool() // call function
+    .then(()=>console.log("Database connected sucesfully")) //if successful connection print message in clg
+    .catch((err:any)=>console.log("Database connection failed",err)) //else if error (no succesful connection), print mesage in clg and show type of error encountered
